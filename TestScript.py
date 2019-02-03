408552e73673ce781ccf989a8b78c13d497f16b5
Dangeecko <poidydaniel@gmail.com>
2019-02-03 02:35:10 +0000

    Ajout de tests des valeurs saisies par l'utilisateur

--- TestScript.py
+++ TestScript.py
@@ -1,4 +1,10 @@
 #Création de la première variable
-
 surnom = input("Entrez votre surnom : ")
-print("Merci", surnom)
+
+#Testons les valeurs saisies par l'utilisateur
+if surnom == "":
+    print("Vous devez absolument saisir votre surnom")
+elif not surnom.isalpha():
+    print("Vous devez saisir une suite de lettres formant votre surnom")
+else :
+    print("Bonjour",surnom, "merci d'avoir tester mon script")
