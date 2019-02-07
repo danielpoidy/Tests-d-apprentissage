e2853b5a55d82e6a2a173b1c65edb549caff3593
Dangeecko <poidydaniel@gmail.com>
2019-02-07 12:36:20 +0000

    petite modification du code

--- TestScript.py
+++ TestScript.py
@@ -1,15 +1,12 @@
-#Créons une autre variable pour décider du fonctionnement ou de l'arret du scrip
+# Créons une autre variable pour décider du fonctionnement ou de l'arret du scrip
 FonctionnementScript = 0
-#Création de la première variable
-#surnom = input("Entrez votre surnom : ")
-
-#Testons les valeurs saisies par l'utilisateur
-while FonctionnementScript == 0 :
+# Testons les valeurs saisies par l'utilisateur
+while FonctionnementScript == 0:
     surnom = input("Entrez votre surnom : ")
     if not surnom.isalpha():
         print("Vous devez saisir une suite de lettres formant votre surnom")
         FonctionnementScript == 0
-    else :
-        print("Bonjour",surnom, "merci d'avoir tester mon script")
+    else:
+        print("Bonjour", surnom + ",", "merci d'avoir tester mon script")
         FonctionnementScript == 1
         break
